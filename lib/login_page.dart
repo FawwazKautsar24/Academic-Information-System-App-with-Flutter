a4776fdb18e14119fdf74d25670d4250c72a01ff
FawwazKats <fawwazkautsar1907912@upi.edu>
2022-10-31 13:33:36 +0700

    Membuat dan menerapkan styling pada button login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -26,13 +26,54 @@ class LoginPage extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(
-                child: Text('Login'),
-                onPressed: () {
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-                    return AccountPage();
-                  },));
-                },
+              Container(
+                margin: EdgeInsets.only(right: 20),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red.shade900, //background color of button
+                      side: BorderSide(width:3, color:Colors.red.shade900), //border width and color
+                      elevation: 3, //elevation of button
+                      shape: RoundedRectangleBorder( //to set border radius to button
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                        padding: EdgeInsets.fromLTRB(30, 20, 30, 20) //content padding inside button
+                  ),
+                  child: Text('Login', 
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  onPressed: () {
+                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                      return AccountPage();
+                    },));
+                  },
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white, //background color of button
+                      side: BorderSide(width:3, color:Colors.red.shade900), //border width and color
+                      elevation: 3, //elevation of button
+                      shape: RoundedRectangleBorder( //to set border radius to button
+                                borderRadius: BorderRadius.circular(25)
+                            ),
+                        padding: EdgeInsets.fromLTRB(30, 20, 30, 20) //content padding inside button
+                  ),
+                  child: Text('Register', 
+                    style: TextStyle(
+                        color: Colors.red.shade900,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      return AccountPage();
+                    },));
+                  },
+                ),
               )
             ]
           )
