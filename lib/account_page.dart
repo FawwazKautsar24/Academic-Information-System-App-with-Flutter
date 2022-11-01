317dee05275ea8cf7e3ec83122c14993cf994589
FawwazKats <fawwazkautsar1907912@upi.edu>
2022-11-01 11:05:54 +0700

    Membuat tampilan AppBar pada account_page

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:simple_shopping_mall_app_with_flutter/account_editProfile_page.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
 
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account Page')
+        // title: Text('Account Page'),
+        title: Column(
+          children: <Widget>[
+            Text('Account Page'),
+            Container(
+              width: 70,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100.0),
+                child: Image.asset(
+                  'no-profile.png', 
+                ),
+              ),
+            ),
+          ]
+        ),
+        backgroundColor: Colors.red.shade900,
+        toolbarHeight: 120,
+        actions: [
+          Icon(
+            Icons.edit_note, 
+            size: 35
+          ),
+        ],
       ),
       body: Center(
         child: ElevatedButton(
